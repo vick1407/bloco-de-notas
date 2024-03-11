0f78e6e9969189407ee009dcedd42832f7fc1783
vitoria mattos borges <vitoria-borges6@educar.rs.gov.br>
2024-03-11 16:39:58 -0300

    sla

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
 let addNote = document.querySelector('#add-note');//Botão de para adicionar nota
-let btncloseModal =  document.querySelector('#btn-close-modal'); //fechar janela modal com os detalhes da nota.
+let btnCloseModal =  document.querySelector('#btn-close-modal'); //fechar janela modal com os detalhes da nota.
 let modal = document.querySelector('#modal'); //Modal para edição das notas
 let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
-let notes = document.querySelector('.item-notes');//Lista divs com dados das notas
-let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
-//let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
+let notes = document.querySelectorAll('.item-note');//Lista divs com dados das notas
+let btnSaveModal = document.querySelector("#btn-save-modal"); //icone para salvar nota
 
-addNote.addEventListener("click", evt =>{
+addNote.addEventListener("click", (evt) => {
     evt.preventDefault();
-    modal.style.dislpay = "block";
+    modal.style.display = "block";
     addNote.style.display = "none";
     notes.style.display = "none";
 });
 
-btncloseModal.addEventListener("click", evt => {
+btnCloseModal.addEventListener("click", (evt) => {
     evt.preventDefault();
     modal.style.display = "none";
     addNote.style.display = "block";
     notes.style.display = "flex";
+});
 
+btnSaveModal.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    data = {
+        id: document.querySelector("#input-id").value,
+        title: document.querySelector("#input-title").value,
+        content: document.querySelector("#input-content").value
+    }
 
-})
+    SaveModal(data);
+
+});
+
+const SaveModal = (note) => {
+    console.log(note);
+};
