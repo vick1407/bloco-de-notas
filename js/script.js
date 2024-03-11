641c52bd3576459877a99be49edd5211d257ffc0
vitoria mattos borges <vitoria-borges6@educar.rs.gov.br>
2024-03-11 16:03:36 -0300

    a

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 let addNote = document.querySelector('#add-note');//Botão de para adicionar nota
-let closeModal =  document.querySelector('#close-modal'); //fechar janela modal com os detalhes da nota.
+let btncloseModal =  document.querySelector('#btn-close-modal'); //fechar janela modal com os detalhes da nota.
 let modal = document.querySelector('#modal'); //Modal para edição das notas
 let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
 let notes = document.querySelector('.item-notes');//Lista divs com dados das notas
 let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
-let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
+//let btnCloseNote = document.querySelector("#btn-close-note");//icone para fechar modal de edição de nota.
 
 addNote.addEventListener("click", evt =>{
     evt.preventDefault();
@@ -12,3 +12,12 @@ addNote.addEventListener("click", evt =>{
     addNote.style.display = "none";
     notes.style.display = "none";
 });
+
+btncloseModal.addEventListener("click", evt => {
+    evt.preventDefault();
+    modal.style.display = "none";
+    addNote.style.display = "block";
+    notes.style.display = "flex";
+
+
+})
