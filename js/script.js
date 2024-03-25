f3035266a13825b6adb16559b4d4c94840bbe725
vitoria mattos borges <vitoria-borges6@educar.rs.gov.br>
2024-03-25 16:24:04 -0300

    a

--- js/script.js
+++ js/script.js
@@ -4,180 +4,105 @@ let modal = document.querySelector('#modal'); //Modal para edição das notas
 let modalView = document.querySelector('#modal-view'); //Modal para exibição dos detalhes da nota
 let notes = document.querySelector('#notes');//Lista divs com dados das notas
 let btnSaveNote = document.querySelector("#btn-save-note"); //icone para salvar nota
-//let btnCloseNote = document.querySelector("#btn-close-note"); //icone para fechar modal de edição de nota.
 
-
-
-
-
-
-
-
-/* ===================== EVENTOS  =================================*/
-addNote.addEventListener("click", (evt) =>{
+addNote.addEventListener("click", (evt) => {
     evt.preventDefault();
     modal.style.display = "block";
-    addNote.style.display ="none";
+    addNote.style.display = "none";
     notes.style.display = "none";
-} );
-btnCloseModal.addEventListener("click", (evt) =>{
+});
+
+btnCloseModal.addEventListener("click", (evt) => {
     evt.preventDefault();
     modal.style.display = "none";
-    addNote.style.display ="block";
-    notes.style.display = "flex";    
+    addNote.style.display = "block";
+    notes.style.display = "flex";
 });
-btnSaveNote.addEventListener("click", (evt) =>{
+
+btnSaveNote.addEventListener("click", (evt) => {
     evt.preventDefault();
     data = {
-    id : document.querySelector("#input-id").value,
-    title: document.querySelector("#input-title").value,
-    content: document.querySelector("#input-content").value
+        id: document.querySelector("#input-id").value,
+        title: document.querySelector("#input-title").value,
+        content: document.querySelector("#input-content").value
     }
-saveNote(data);
+
+    SaveNote(data);
+
 });
 
-
-
-
-/* ===================== FUNÇÕES  =================================*/
-
-
-
-
-const saveNote = (note) => {
-
-
-
-
+const SaveNote = (note) => {
     let notes = loadNotes();
 
-
-
-
-    if(note.id.trim().length < 1){ //para remover espaços (sujeiras) usa-se o trim().
+    if(note.id.trim().length < 1){
         note.id = new Date().getTime();
-    }else{
-   
     }
 
-
-
-
     note.lastTime = new Date().getTime();
+
     console.log(note);
-    notes.push(note);
 
+    note.push(note);
 
+    notes = JSON.stringify(notes);
 
-
-    notes = JSON.stringify(notes); //transformar objeto em texto novamente
-
-
-
-
-    localStorage.setItem('notes', notes); //colocar o texto no local storage
-
-
-
+    localStorage.setItem('notes', notes);
 
 };
 
-
-
-
-const loadNotes = () =>{
-
-
-
-
+const loadNotes = () => {
     let notes = localStorage.getItem('notes');
-
-
-
 
     if(!notes){
         notes = [];
-    }else{
-        notes = JSON.parse(notes);} //uso do parse: transformação do notes em forma de string para objeto
-  return notes;  
-
-
-
-
+    }
+    else{
+        notes = JSON.parse(notes);
+    }
+    return notes;
 }
 
-
-
-
-const listNotes = () =>{
+const listNotes =  () =>{
     let listNotes = loadNotes();
-    listNotes.forEach((note) => {
+    listNotes.forEach((note) =>{
         let divCard = document.createElement('div');
         divCard.className = 'card';
         divCard.style.width = '25rem';
-        let divCardBody = document.createElement('div');
-        divCardBody.className = 'cardBody';
+        let divCardBody = document.createElement('div')
+        divCardBody.className ='card-body';
         divCard.appendChild(divCardBody);
         let h5 = document.createElement('h5');
         h5.innerText = note.title;
         divCardBody.appendChild(h5);
-        let paragrafo = document.createElement('p');
-        paragrafo.innerText = note.content;
-        divCardBody.appendChild(paragrafo);
-        let time = new Date(note.lastTime);
-        time = time.toLocaleDateString("pt-BR");
-        console.log(time);
-        let pData = document.createElement('p');
-        pData.innerText = time;
-        divCardBody.appendChild(pData);
-
-
-
-
+        let pContent = document.createElement('p');
+        pContent.innerText = note.content;
+        let pLastTime = document.createElement('p');
+        
+        pLastTime.innerText = "Atualizado em: " +dateFormat (note.lastTime);
+        
+        divCardBody.appendChild(pContent);
+        divCardBody.appendChild(pLastTime);
+        
         notes.appendChild(divCard);
-    });
+    };
 
+        const showNote = (note) =>{
+            
+            notes.style.display = 'none';
+            addNote.style.display = 'none';
+            modalView.style.display = 'block';
+            document.querySelector('#title-note').innerHTML = "<h1>"+note.title+"</h1>";
+            document.querySelector("#content-note").innerHTML = '<p>Ultima Alteração : $(dateFormat('note.lastTime)</p>'
+        };
 
-
-    const listNotes = () =>{
-        let listNotes = loadNotes();
-        listNotes.forEach((note) => {
-            let divCard = document.createElement('div');
-            divCard.className = 'card';
-            divCard.style.width = '25rem';
-            let divCardBody = document.createElement('div');
-            divCardBody.className = 'cardBody';
-            divCard.appendChild(divCardBody);
-            let h5 = document.createElement('h5');
-            h5.innerText = note.title;
-            divCardBody.appendChild(h5);
-            let paragrafo = document.createElement('p');
-            paragrafo.innerText = note.content;
-            divCardBody.appendChild(paragrafo);
-            let time = new Date(note.lastTime);
-            time = time.toLocaleDateString("pt-BR");
-            console.log(time);
-            let pData = document.createElement('p');
-            pData.innerText = time;
-            divCardBody.appendChild(pData);
-    
-    
-    
-    
-    
-    
-    
-    
-            notes.appendChild(divCard);
-        });
-    }
-    listNotes();
-    
-    
-    
+const dateFormat = (timestamp) =>{
+    let datetime = new Date (note.lastTime);
+        datetime = datetime.toLocaleDateString("pt-BR");
+        pLastTime.innerText = "Atualizado em: "+datetime;
+        return date;
 }
 
+        
+    });
 
-
-
-
+}
